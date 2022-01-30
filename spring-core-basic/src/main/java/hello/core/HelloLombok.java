2d82f2976d49911fc37535f181c34b82553449db
LeeJejune <leejejune0625@gmail.com>
2022-01-30 14:54:17 +0900

    feat: lombok settings

---
+++ spring-core-basic/src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdadsa");
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
--- spring-core-basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core-basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,34 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // 수정자 주입 방법 (setter 주입 방법)
-    // private MemberRepository memberRepository;
-    // private DiscountPolicy discountPolicy;
-
-
-    //@Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
+    // @Autowired 생성자가 1개 일때는 생략해도 상관 없음. lombok은 알아서 만들어줌.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 //        this.memberRepository = memberRepository;
-//    }
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        this.discountPolicy = discountPolicy;
 //    }
-
-    // @Autowired 생성자가 1개 일때는 생략해도 상관 없음.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
