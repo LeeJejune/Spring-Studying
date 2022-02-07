f06e8537f461f362b8b1b47f33f75ace49a13e88
LeeJejune <leejejune0625@gmail.com>
2022-02-07 17:06:09 +0900

    add: type collections example

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,22 +14,38 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            // JPA - insert -> persist
-            // JPA - update -> set~
-            // JPA - delete -> remove
-            // JPA - select -> find
 
-            //Member findMember = em.find(Member.class, 1L);
-            //findMember.setName("HelloJPA");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "19999", "street"));
 
-            //JPQL 사용
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntiry("old1", "19999", "street"));
+            member.getAddressHistory().add(new AddressEntiry("old2", "19999", "street"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=============start==========");
+            Member findMember = em.find(Member.class, member.getId());
+
+            //homeCity -> newCity
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getZipcode(), a.getStreet()));
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+//            주소
+//            findMember.getAddressHistory().remove(new Address("old1", "19999", "street"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "19999", "street"));
+
             tx.commit();
 
         } catch (Exception e){
