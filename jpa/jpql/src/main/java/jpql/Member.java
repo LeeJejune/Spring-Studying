1d394acdc6882399d6ca79561ef42e3b7fea49ea
LeeJejune <leejejune0625@gmail.com>
2022-02-15 18:38:44 +0900

    add: namedquery entity

--- jpa/jpql/src/main/java/jpql/Member.java
+++ jpa/jpql/src/main/java/jpql/Member.java
@@ -3,6 +3,10 @@ package jpql;
 import javax.persistence.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
     @Id @GeneratedValue
     private Long id;
