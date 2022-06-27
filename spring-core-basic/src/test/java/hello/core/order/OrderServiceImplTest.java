764ce4c060daa4c85d2d1981c010bfe303df5542
LeeJejune <leejejune0625@gmail.com>
2022-06-27 13:55:54 +0900

    initial commit :)

---
+++ spring-core-basic/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
