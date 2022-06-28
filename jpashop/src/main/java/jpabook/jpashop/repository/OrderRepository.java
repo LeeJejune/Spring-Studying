4069eb522ba131f5ae66c294bee263126fcaa60c
LeeJejune <leejejune0625@gmail.com>
2022-06-28 17:47:53 +0900

    feat: Order, Item repository 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch){}
+}
